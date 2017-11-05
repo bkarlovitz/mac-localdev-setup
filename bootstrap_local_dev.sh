25fa7a84ac398703b990ca1fb39e0b02309bb69d
Bryan Karlovitz <bryankarlovitz@Bryans-MacBook-Pro.local>
2017-11-05 17:02:20 -0600

    bootstrap java8 and intellij

---
+++ bootstrap_local_dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the latest version of Homebrew.
+echo "Updating Homebrew ..."
+brew update
+
+# Get the latest version of Hombrew Cask.
+echo "Updating Homebrew Cask ..."
+brew tap caskroom/cask
+
+# Get the Homebrew Versions Cask (necessary for installing java 8)
+echo "Updating Homebrew Versions Cask ..."
+brew tap caskroom/versions
+
+# Get the latest version of Ansible.
+echo "Updating Ansible ..."
+brew install ansible
+
+# Run the local dev Ansible playbook.
+ansible-playbook -i inventories/development playbooks/setup-localdev.yml
