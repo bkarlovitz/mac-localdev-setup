07b63092507df35bf939df5b15b228c7b26306ee
bkarlovitz <bryan.a.karlovitz@gmail.com>
2017-11-14 15:01:51 -0600

    refactor homebrew install/upgrade logic

--- bootstrap_local_dev.sh
+++ bootstrap_local_dev.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Get the latest version of Homebrew.
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Installing Homebrew ..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "Installing Homebrew Cask ..."
+    brew tap caskroom/cask
+
+    echo "Installing Homebrew Versions Cask ..."
+    brew tap caskroom/versions  # used to get java 8
+fi
+
 echo "Updating Homebrew ..."
-brew update
-
-# Get the latest version of Hombrew Cask.
-echo "Updating Homebrew Cask ..."
-brew tap caskroom/cask
-
-# Get the Homebrew Versions Cask (necessary for installing java 8)
-echo "Updating Homebrew Versions Cask ..."
-brew tap caskroom/versions
+brew update   # this also updates homebrew cask and versions
 
 # Get the latest version of Ansible.
 echo "Updating Ansible ..."
 brew install ansible
+
 
 # Run the local dev Ansible playbook.
 ansible-playbook --ask-become-pass -i inventories/development playbooks/setup-localdev.yml
